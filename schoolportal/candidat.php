c242afb3d190ff84c683ee180f1e7a888d4b7781
BelousovNG <99665330+BelousovNG@users.noreply.github.com>
2022-02-24 14:15:46 +0300

    Update candidat.php

--- schoolportal/candidat.php
+++ schoolportal/candidat.php
@@ -8,6 +8,17 @@
     $candidate = $mysql->query($sql)->fetch_assoc();
    <br>
     ?>
+<?php
+$id = $_POST['id_candidate'];
+$mysql = new mysqli('localhost', 'root', '', 'portal');
+$sql = "SELECT `votes` FROM `candidates` WHERE `id`='$id'";
+$old_votes = $mysql->query($sql)->fetch_assoc();
+$new_votes = (int)$old_votes['votes'];
+$new_votes++;
+$sql = "UPDATE `candidates` SET `votes`=$new_votes WHERE `id`='$id'";
+$mysql->query($sql);
+echo $new_votes;
+
     <main>
         <div class="container">
             <div class="info">
